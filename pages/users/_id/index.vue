2dca2fc35f36dad165cba99561bd45684a80374a
Leandro <vitto.leandro@gmail.com>
2019-09-20 17:48:55 +0200

    Init Validation

--- pages/users/_id/index.vue
+++ pages/users/_id/index.vue
@@ -3,3 +3,15 @@
     <h1>Utente: {{ $route.params.id }}</h1>
   </div>
 </template>
+
+
+<script>
+
+export default {
+  validate(data){
+    console.log(data);
+    return true;
+  }
+}
+
+</script>
