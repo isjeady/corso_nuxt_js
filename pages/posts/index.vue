78b11070e57cf057f443e8febabede1f7b03b233
Leandro <vitto.leandro@gmail.com>
2019-09-21 11:21:31 +0200

    Post Cycle

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -2,14 +2,14 @@
 <div>
     <p class="text-5xl">Posts</p>
     <div class="flex flex-wrap justify-between pt-12 -mx-6">
-			<div class="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
+			<div v-for="(post,index) in posts" class="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink" :key="`_post_${index}`">
 						<PostPreview
-              id="1"
-              title="Lorem ipsum dolor sit amet."
-              description="	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at i"
-              thumbnail="https://source.unsplash.com/collection/225/800x600"
-              userImg="http://i.pravatar.cc/300"
-              timeToRead="1 MIN READ"
+              :id="post.id"
+              :title="post.title"
+              :description="post.description"
+              :thumbnail="post.thumbnail"
+              :userImg="post.userImg"
+              :timeToRead="post.timeToRead"
             ></PostPreview>
 		  </div>
 		</div>
@@ -27,12 +27,14 @@ export default {
   data(){
     return {
       posts : [
-        { id : '1', title : 'Lorem ipsum dolor sit amet.',description : 'Lorem ipsum eu nunc commodo posuere et sit amet ligula.' ,thumbnail : '225', userImg : 'http://i.pravatar.cc/300', timeToRead : '1 MIN READ'},
-        { id : '2', title : 'Lorem ipsum dolor sit amet.',description : 'Lorem ipsum eu nunc commodo posuere et sit amet ligula.' ,thumbnail : '3106804', userImg : 'http://i.pravatar.cc/300', timeToRead : '1 MIN READ'},
-        { id : '3', title : 'Lorem ipsum dolor sit amet.',description : 'Lorem ipsum eu nunc commodo posuere et sit amet ligula.' ,thumbnail : '3657445', userImg : 'http://i.pravatar.cc/300', timeToRead : '1 MIN READ'},
-        { id : '4', title : 'Lorem ipsum dolor sit amet.',description : 'Lorem ipsum eu nunc commodo posuere et sit amet ligula.' ,thumbnail : '764827', userImg : 'http://i.pravatar.cc/300', timeToRead : '1 MIN READ'},
-        { id : '5', title : 'Lorem ipsum dolor sit amet.',description : 'Lorem ipsum eu nunc commodo posuere et sit amet ligula.' ,thumbnail : '1118905', userImg : 'http://i.pravatar.cc/300', timeToRead : '1 MIN READ'},
-        { id : '6', title : 'Lorem ipsum dolor sit amet.',description : 'Lorem ipsum eu nunc commodo posuere et sit amet ligula.' ,thumbnail : '325867', userImg : 'http://i.pravatar.cc/300', timeToRead : '1 MIN READ'}
+        { id : '1', title : 'Lorem ipsum dolor sit amet.',description : 'Lorem ipsum eu nunc commodo posuere et sit amet ligula.' ,thumbnail : 'https://source.unsplash.com/collection/225/800x600', userImg : 'http://i.pravatar.cc/300', timeToRead : '1 MIN READ'},
+        { id : '2', title : 'Lorem ipsum dolor sit amet.',description : 'Lorem ipsum eu nunc commodo posuere et sit amet ligula.' ,thumbnail : 'https://source.unsplash.com/collection/3106804/800x600', userImg : 'http://i.pravatar.cc/300', timeToRead : '1 MIN READ'},
+        { id : '3', title : 'Lorem ipsum dolor sit amet.',description : 'Lorem ipsum eu nunc commodo posuere et sit amet ligula.' ,thumbnail : 'https://source.unsplash.com/collection/3657445/800x600', userImg : 'http://i.pravatar.cc/300', timeToRead : '1 MIN READ'},
+        { id : '4', title : 'Lorem ipsum dolor sit amet.',description : 'Lorem ipsum eu nunc commodo posuere et sit amet ligula.' ,thumbnail : 'https://source.unsplash.com/collection/764827/800x600', userImg : 'http://i.pravatar.cc/300', timeToRead : '1 MIN READ'},
+        { id : '5', title : 'Lorem ipsum dolor sit amet.',description : 'Lorem ipsum eu nunc commodo posuere et sit amet ligula.' ,thumbnail : 'https://source.unsplash.com/collection/1118905/800x600', userImg : 'http://i.pravatar.cc/300', timeToRead : '1 MIN READ'},
+        { id : '6', title : 'Lorem ipsum dolor sit amet.',description : 'Lorem ipsum eu nunc commodo posuere et sit amet ligula.' ,thumbnail : 'https://source.unsplash.com/collection/325867/800x600', userImg : 'http://i.pravatar.cc/300', timeToRead : '1 MIN READ'},
+        { id : '6', title : 'Lorem ipsum dolor sit amet.',description : 'Lorem ipsum eu nunc commodo posuere et sit amet ligula.' ,thumbnail : 'https://source.unsplash.com/collection/3657445/800x600', userImg : 'http://i.pravatar.cc/300', timeToRead : '1 MIN READ'},
+        { id : '6', title : 'Lorem ipsum dolor sit amet.',description : 'Lorem ipsum eu nunc commodo posuere et sit amet ligula.' ,thumbnail : 'https://source.unsplash.com/collection/764827/800x600', userImg : 'http://i.pravatar.cc/300', timeToRead : '1 MIN READ'}
       ]
     }
   }
