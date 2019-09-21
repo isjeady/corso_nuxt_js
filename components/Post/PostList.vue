4251321f60844e2206b01cd03f958ae06896663c
Leandro <vitto.leandro@gmail.com>
2019-09-21 12:16:57 +0200

    Post List

---
+++ components/Post/PostList.vue
@@ -0,0 +1,67 @@
+<template>
+<div>
+  <div class="flex flex-wrap justify-between pt-12 -mx-6">
+			<div v-for="(post,index) in posts" :class="`w-full md:w-${post.tail} p-6 flex flex-col flex-grow flex-shrink`" :key="`_post_${index}`">
+						<PostPreview
+              :id="post.id"
+              :title="post.title"
+              :description="post.description"
+              :thumbnail="post.thumbnail"
+              :userImg="post.userImg"
+              :timeToRead="post.timeToRead"
+              :tail="post.tail"
+            ></PostPreview>
+		  </div>
+	</div>
+</div>
+</template>
+
+
+<script>
+
+import PostPreview from '~/components/Post/PostPreview.vue'
+
+export default {
+  name: 'PostList',
+  components: {
+    PostPreview
+  },
+  data(){
+    return {
+      posts : [],
+      allPosts : [
+        { tail : '1/3', id : '1', title : 'Lorem ipsum dolor sit amet.',description : 'Lorem ipsum eu nunc commodo posuere et sit amet ligula.' ,thumbnail : 'https://source.unsplash.com/collection/225/800x600', userImg : 'http://i.pravatar.cc/300', timeToRead : '1 MIN READ'},
+        { tail : '1/3', id : '2', title : 'Lorem ipsum dolor sit amet.',description : 'Lorem ipsum eu nunc commodo posuere et sit amet ligula.' ,thumbnail : 'https://source.unsplash.com/collection/3106804/800x600', userImg : 'http://i.pravatar.cc/300', timeToRead : '1 MIN READ'},
+        { tail : '1/3', id : '3', title : 'Lorem ipsum dolor sit amet.',description : 'Lorem ipsum eu nunc commodo posuere et sit amet ligula.' ,thumbnail : 'https://source.unsplash.com/collection/3657445/800x600', userImg : 'http://i.pravatar.cc/300', timeToRead : '1 MIN READ'},
+        { tail : '1/2', id : '4', title : 'Lorem ipsum dolor sit amet.',description : 'Lorem ipsum eu nunc commodo posuere et sit amet ligula.' ,thumbnail : 'https://source.unsplash.com/collection/764827/800x600', userImg : 'http://i.pravatar.cc/300', timeToRead : '1 MIN READ'},
+        { tail : '1/2', id : '5', title : 'Lorem ipsum dolor sit amet.',description : 'Lorem ipsum eu nunc commodo posuere et sit amet ligula.' ,thumbnail : 'https://source.unsplash.com/collection/1118905/800x600', userImg : 'http://i.pravatar.cc/300', timeToRead : '1 MIN READ'},
+        { tail : '1/3', id : '6', title : 'Lorem ipsum dolor sit amet.',description : 'Lorem ipsum eu nunc commodo posuere et sit amet ligula.' ,thumbnail : 'https://source.unsplash.com/collection/325867/800x600', userImg : 'http://i.pravatar.cc/300', timeToRead : '1 MIN READ'},
+        { tail : '1/3', id : '6', title : 'Lorem ipsum dolor sit amet.',description : 'Lorem ipsum eu nunc commodo posuere et sit amet ligula.' ,thumbnail : 'https://source.unsplash.com/collection/3657445/800x600', userImg : 'http://i.pravatar.cc/300', timeToRead : '1 MIN READ'},
+        { tail : '1/3', id : '6', title : 'Lorem ipsum dolor sit amet.',description : 'Lorem ipsum eu nunc commodo posuere et sit amet ligula.' ,thumbnail : 'https://source.unsplash.com/collection/764827/800x600', userImg : 'http://i.pravatar.cc/300', timeToRead : '1 MIN READ'}
+      ],
+      lastPosts : [
+        { tail : '1/2', id : '4', title : 'Lorem ipsum dolor sit amet.',description : 'Lorem ipsum eu nunc commodo posuere et sit amet ligula.' ,thumbnail : 'https://source.unsplash.com/collection/764827/800x600', userImg : 'http://i.pravatar.cc/300', timeToRead : '1 MIN READ'},
+        { tail : '1/2', id : '5', title : 'Lorem ipsum dolor sit amet.',description : 'Lorem ipsum eu nunc commodo posuere et sit amet ligula.' ,thumbnail : 'https://source.unsplash.com/collection/1118905/800x600', userImg : 'http://i.pravatar.cc/300', timeToRead : '1 MIN READ'},
+        { tail : '1/2', id : '6', title : 'Lorem ipsum dolor sit amet.',description : 'Lorem ipsum eu nunc commodo posuere et sit amet ligula.' ,thumbnail : 'https://source.unsplash.com/collection/325867/800x600', userImg : 'http://i.pravatar.cc/300', timeToRead : '1 MIN READ'},
+        { tail : '1/2', id : '6', title : 'Lorem ipsum dolor sit amet.',description : 'Lorem ipsum eu nunc commodo posuere et sit amet ligula.' ,thumbnail : 'https://source.unsplash.com/collection/3657445/800x600', userImg : 'http://i.pravatar.cc/300', timeToRead : '1 MIN READ'},
+      ]
+    }
+  },
+  mounted(){
+    if(this.type == 'all'){
+      this.posts = this.allPosts;
+    }else if(this.type == 'last'){
+      this.posts = this.lastPosts;
+    }
+  },
+  methods : {
+
+  },
+  props: {
+    type: {
+      type : String,
+      required: true
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,16 @@
 <template>
   <div>
-    <div class="flex h-full bg-white rounded overflow-hidden shadow-lg">
-					<a href="post.html" class="flex flex-wrap no-underline hover:no-underline">
-						<div class="w-full md:w-2/3 rounded-t">	
-							<img src="https://source.unsplash.com/collection/494263/800x600" class="h-full w-full shadow">
-						</div>
-
-						<div class="w-full md:w-1/3 flex flex-col flex-grow flex-shrink">
-							<div class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow-lg">
-								<p class="w-full text-gray-600 text-xs md:text-sm pt-6 px-6">GETTING STARTED</p>
-								<div class="w-full font-bold text-xl text-gray-900 px-6">👋 Welcome fellow Tailwind CSS and Ghost fan</div>
-								<p class="text-gray-800 font-serif text-base px-6 mb-5">
-									This starter template is an attempt to replicate the default Ghost theme "Casper" using Tailwind CSS and vanilla Javascript.
-								</p>
-							</div>
-
-							<div class="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow-lg p-6">
-								<div class="flex items-center justify-between">
-									<img class="w-8 h-8 rounded-full mr-4 avatar" data-tippy-content="Author Name" src="http://i.pravatar.cc/300" alt="Avatar of Author" tabindex="0">
-									<p class="text-gray-600 text-xs md:text-sm">1 MIN READ</p>
-								</div>
-							</div>
-						</div>
-
-					</a>
-				</div>
+    <p class="text-white font-extrabold text-3xl md:text-5xl">Home</p>
+    <PostList type="last" />
   </div>
-
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import PostList from '~/components/Post/PostList.vue'
 
 export default {
   components: {
-    Logo
+    PostList
   }
 }
 </script>
