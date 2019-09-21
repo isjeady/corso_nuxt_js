7706dcd8ecc228bc2951f8c83717b6544a5ec136
Leandro <vitto.leandro@gmail.com>
2019-09-21 10:55:13 +0200

    About

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,9 +1,15 @@
 <template>
-  <div class="container">
-    <div>
-      <h1 class="title">
-       ABOUT
-      </h1>
-    </div>
+  <div>
+    <p class="text-5xl">About</p>
+    <p class="text-2xl">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim, tempora itaque, vitae, ducimus recusandae
+      iusto laborum inventore voluptates possimus ad explicabo earum corporis velit id cupiditate aliquam sequi
+      molestias temporibus!
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque molestias itaque sunt distinctio id. Illo nesciunt reprehenderit, excepturi quis quod tempore. Fugit, iure. Inventore, suscipit pariatur et vel magnam facilis?
+      Nostrum architecto sequi quam dolorum quaerat, similique ad dolore nulla aliquid totam quos nam asperiores assumenda minus deserunt sit blanditiis fuga sed molestias corporis. Inventore ipsam et est doloribus omnis.
+      Tenetur et odit laborum quis. In vero magnam incidunt voluptas. Voluptas ad eligendi numquam labore vel iste non ratione at dicta autem voluptatem voluptate nesciunt illo, consequuntur cupiditate officiis. Natus.
+      Dolore harum architecto veritatis, soluta quod nemo molestiae cum impedit. Minima, nisi enim libero aperiam aspernatur perferendis assumenda sapiente repellat! Eum incidunt libero, earum sint corrupti doloribus nihil est qui.
+      Optio illo nisi at placeat, magni eos voluptatem nulla! Distinctio repudiandae ad officiis soluta totam molestias tempore natus nesciunt. Amet rem voluptate labore libero aspernatur nemo expedita delectus pariatur ipsum.
+    </p>
   </div>
 </template>
