26c6dcb6a8e803ff953ab397338e872156a10a08
Leandro <vitto.leandro@gmail.com>
2019-09-21 18:02:33 +0200

    PostPage e Error

---
+++ layouts/error.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <h1 style="font-size:70px">ERROR</h1>
+    <h1 style="font-size:200px">404</h1>
+  </div>
+</template>
+
+
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
--- pages/posts/_id.vue
+++ pages/posts/_id.vue
@@ -1,9 +1,28 @@
 <template>
-  <div class="container">
-    <div>
-      <h1 class="title">
-         POST : {{ $route.params.id }}
-      </h1>
-    </div>
-  </div>
+<div>
+  <div class="container max-w-5xl mx-auto -mt-32">
+
+		<div class="mx-0 sm:mx-6">
+
+			<div class="bg-white w-full p-8 md:p-24 text-xl md:text-2xl text-gray-800 leading-normal" style="font-family:Georgia,serif;">
+
+   <p class="font-extrabold text-3xl md:text-5xl">Posts  {{ $route.params.id }}</p>
+
+		<p class="text-sm md:text-base text-teal-500 font-bold">08 APRIL 2019 <span class="text-gray-900">/</span> GETTING STARTED</p>
+
+				<p class="py-6 text-left">The basic blog page layout is available and all using the default Tailwind CSS classes (although there are a few hardcoded style tags). If you are going to use this in your project, you will want to convert the classes into components.</p>
+
+				<p class="py-6 text-left">Sed dignissim lectus ut tincidunt vulputate. Fusce tincidunt lacus purus, in mattis tortor sollicitudin pretium. Phasellus at diam posuere, scelerisque nisl sit amet, tincidunt urna. Cras nisi diam, pulvinar ut molestie eget, eleifend ac magna. Sed at lorem condimentum, dignissim lorem eu, blandit massa. Phasellus eleifend turpis vel erat bibendum scelerisque. Maecenas id risus dictum, rhoncus odio vitae, maximus purus. Etiam efficitur dolor in dolor molestie ornare. Aenean pulvinar diam nec neque tincidunt, vitae molestie quam fermentum. Donec ac pretium diam. Suspendisse sed odio risus. Nunc nec luctus nisi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis nec nulla eget sem dictum elementum.</p>
+
+				<blockquote class="border-l-4 border-teal-500 italic my-8 pl-8 md:pl-12">Example of blockquote - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at ipsum eu nunc commodo posuere et sit amet ligula.</blockquote>
+			</div>
+
+		</div>
+
+
+	</div>
+
+</div>
 </template>
+
+
