380491ee3b84cadfba60c59e1f64cbe91d6e2c1c
Leandro <vitto.leandro@gmail.com>
2019-10-01 00:01:07 +0200

    Async DAta

--- pages/posts/_id.vue
+++ pages/posts/_id.vue
@@ -10,12 +10,13 @@
 
 		<p class="text-sm md:text-base text-teal-500 font-bold">08 APRIL 2019 <span class="text-gray-900">/</span> GETTING STARTED</p>
 
-				<p class="py-6 text-left">The basic blog page layout is available and all using the default Tailwind CSS classes (although there are a few hardcoded style tags). If you are going to use this in your project, you will want to convert the classes into components.</p>
+				
 
-				<p class="py-6 text-left">Sed dignissim lectus ut tincidunt vulputate. Fusce tincidunt lacus purus, in mattis tortor sollicitudin pretium. Phasellus at diam posuere, scelerisque nisl sit amet, tincidunt urna. Cras nisi diam, pulvinar ut molestie eget, eleifend ac magna. Sed at lorem condimentum, dignissim lorem eu, blandit massa. Phasellus eleifend turpis vel erat bibendum scelerisque. Maecenas id risus dictum, rhoncus odio vitae, maximus purus. Etiam efficitur dolor in dolor molestie ornare. Aenean pulvinar diam nec neque tincidunt, vitae molestie quam fermentum. Donec ac pretium diam. Suspendisse sed odio risus. Nunc nec luctus nisi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis nec nulla eget sem dictum elementum.</p>
+				<p class="py-6 text-left">
+					{{ currentPost }}
+				</p>
 
-				<blockquote class="border-l-4 border-teal-500 italic my-8 pl-8 md:pl-12">Example of blockquote - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at ipsum eu nunc commodo posuere et sit amet ligula.</blockquote>
-			</div>
+				</div>
 
 		</div>
 
@@ -26,3 +27,25 @@
 </template>
 
 
+<script>
+import PostList from '~/components/Post/PostList.vue'
+
+export default {
+  asyncData(context,callback){
+	  console.log('asyncData');
+	  setTimeout(() => {
+		  callback(null, {
+			  currentPost : {
+				   tail : '1/2', 
+				   id : '4', 
+				   title : 'Lorem ipsum dolor sit amet.',
+				   description : 'Lorem ipsum eu nunc commodo posuere et sit amet ligula.' ,
+				   thumbnail : 'https://source.unsplash.com/collection/764827/800x600', 
+				   userImg : 'http://i.pravatar.cc/300', 
+				   timeToRead : '1 MIN READ'
+			  }
+		  })
+	  });
+  }
+}
+</script>
