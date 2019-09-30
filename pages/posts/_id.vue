bc7ddb8fad44f6a7307e14261dc9e477f2c7224e
Leandro <vitto.leandro@gmail.com>
2019-10-01 00:03:38 +0200

    Context Params

--- pages/posts/_id.vue
+++ pages/posts/_id.vue
@@ -6,14 +6,14 @@
 
 			<div class="bg-white w-full p-8 md:p-24 text-xl md:text-2xl text-gray-800 leading-normal" style="font-family:Georgia,serif;">
 
-   <p class="font-extrabold text-3xl md:text-5xl">Posts  {{ $route.params.id }}</p>
+   <p class="font-extrabold text-3xl md:text-5xl">Posts  {{ currentPost.id }}</p>
 
 		<p class="text-sm md:text-base text-teal-500 font-bold">08 APRIL 2019 <span class="text-gray-900">/</span> GETTING STARTED</p>
 
 				
-
+	{{ currentPost }}
 				<p class="py-6 text-left">
-					{{ currentPost }}
+					{{ currentPost.description }}
 				</p>
 
 				</div>
@@ -37,7 +37,7 @@ export default {
 		  callback(null, {
 			  currentPost : {
 				   tail : '1/2', 
-				   id : '4', 
+				   id : context.route.params.id, 
 				   title : 'Lorem ipsum dolor sit amet.',
 				   description : 'Lorem ipsum eu nunc commodo posuere et sit amet ligula.' ,
 				   thumbnail : 'https://source.unsplash.com/collection/764827/800x600', 
